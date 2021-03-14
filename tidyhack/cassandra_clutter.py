23ddbd1e34768bdd4271a5013c768c2ee2a93e98
Prasanna Venkatesh <prasannavenkateshramkumar@gmail.com>
2021-03-14 19:12:08 +0530

    Update cassandra_clutter.py

--- tidyhack/cassandra_clutter.py
+++ tidyhack/cassandra_clutter.py
@@ -12,3 +12,11 @@ cloud_config = {
 auth_provider = PlainTextAuthProvider(username=user, password=password)
 cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
 session = cluster.connect()
+
+
+row = session.execute("select * from task.task").one()
+if row:
+    print(row)
+else:
+    print("An error occurred.")
+    print("try checking the credentials once.")
